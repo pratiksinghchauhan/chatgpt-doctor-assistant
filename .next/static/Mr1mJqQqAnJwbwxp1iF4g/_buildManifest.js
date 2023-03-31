d9c7ad1ca6fc0563fb4966ff18646986723fd6f0
pratiksinghchauhan <pratiksingh773@gmail.com>
2023-03-31 20:50:32 +0530

    removed buiild settings

--- .next/static/Mr1mJqQqAnJwbwxp1iF4g/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/955-5a19f3838011c75f.js","static/css/c66d02d86e111836.css","static/chunks/pages/index-859286718eac9246.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
